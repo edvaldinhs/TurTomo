b52dee55b66bf4ec7aeca841b047a9bfb6fe5830
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2023-04-10 16:17:15 -0300

    #1 Automatização do Basico do basico das Rooms/salas

--- app/src/main/java/com/example/turtomo/HomeScreen/RoomsFragment.java
+++ app/src/main/java/com/example/turtomo/HomeScreen/RoomsFragment.java
@@ -2,6 +2,7 @@ package com.example.turtomo.HomeScreen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,18 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.turtomo.HomeScreen.RoomFrag.CustomAdapter;
 import com.example.turtomo.R;
 import com.example.turtomo.HomeScreen.RoomFrag.Room;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RoomsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class RoomsFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +34,6 @@ public class RoomsFragment extends Fragment {
     private String mParam2;
 
     ArrayList<Room> rooms = new ArrayList<>();
-    Button createRoom_btn;
     ListView listView;
 
     public RoomsFragment() {
@@ -62,29 +65,40 @@ public class RoomsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rooms, container, false);
 
-        createRoom_btn = view.findViewById(R.id.createRoom);
         listView = (ListView)view.findViewById(R.id.listView);
 
-        createRoom_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                buildRoomAL();
-                fillListView();
-            }
-        });
+        fillRoomValue();
         return view;
     }
 
-    public void buildRoomAL(){
-        Room r = new Room();
-        String id = "00001";
-        int roomNumber = 1;
-        r = new Room(id, roomNumber);
-        rooms.add(r);
-
-    }
     public void fillListView(){
         CustomAdapter myCustomAdapter = new CustomAdapter(getActivity(), rooms);
         listView.setAdapter(myCustomAdapter);
+    }
+
+    public void fillRoomValue(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("rooms");
+        Query query = reference.orderByChild("id");
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    String id = snapshot.child("id").getValue(String.class);
+                    int roomNumber = snapshot.child("roomNumber").getValue(Integer.class);
+                    Room r = new Room(id, roomNumber);
+                    rooms.add(r);
+                }
+
+                fillListView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity().getApplicationContext(), "Room organizer Error", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
